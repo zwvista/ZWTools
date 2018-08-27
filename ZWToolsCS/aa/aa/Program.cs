5abedf95e23140907c00055f0d3e3894e5415933
zwvista <zwvista@msn.com>
2018-08-27 11:07:00 +0900

    aa 2

--- ZWToolsCS/aa/aa/Program.cs
+++ ZWToolsCS/aa/aa/Program.cs
@@ -25,36 +25,27 @@ namespace aa
             var r3 = new Regex("<.+?>");
             var r4 = new Regex("import .+");
             var r5 = new Regex("(//)(.+)");
-            r.Matches(str).Cast<Match>()
-             .Select(m =>
-             {
+            r.Matches(str).Cast<Match>().Select(m => {
                  var g = m.Groups.Cast<Group>().ToList();
                  return new { Url = g[1].Value, Name = g[2].Value };
-            }).ForEach(o =>
-                client.GetStringAsync(o.Url).ToObservable().SelectMany(str2 =>
-                {
-                    var examples = r.Matches(str2).Cast<Match>()
-                     .Select(m =>
-                     {
-                         var g = m.Groups.Cast<Group>().ToList();
-                         return new { Url = g[1].Value, Name = g[2].Value };
-                     }).ToObservable().SelectMany(o2 =>
-                          client.GetStringAsync(o2.Url).ToObservable().Select(str3 =>
-                          {
-                              var examples2 = r2.Matches(str3).Cast<Match>()
-                                  .Select(m => HttpUtility.HtmlDecode(m.Groups.Cast<Group>().ElementAt(1).Value))
-                                   .SelectMany((s, i) => r5.Replace(r4.Replace(r3.Replace(s, "").Replace("// RxJS v6+\n", ""), ""), "$1 $2")
-                                       .Split('\n').SkipWhile(s2 => string.IsNullOrWhiteSpace(s2))
-                                               .Select(s2 => "    " + s2).StartWith($"  {o2.Name}{i + 1}() {{").Concat(new[] { "  }", "" })
-                                               .ToList())
-                                   .ToList();
-                              return examples2;
-                          })).SelectMany(o2 => o2).ToList();
-                    return examples;
-            }).Subscribe(examples => {
-                File.WriteAllLines($"/Users/zhaowei/Documents/Programs/Tools/{o.Name}.ts", examples);
-                Console.WriteLine($"Finished {o.Name}");
-            }));
+            }).ForEach(o => client.GetStringAsync(o.Url).ToObservable().SelectMany(str2 =>
+                r.Matches(str2).Cast<Match>().Select(m => {
+                    var g = m.Groups.Cast<Group>().ToList();
+                    return new { Url = g[1].Value, Name = g[2].Value };
+                }).ToObservable().Do(o2 =>
+                    Console.WriteLine($@"  <button (click)=""{o.Name}Service.{o2.Name}()"">{o2.Name}</button>")
+                ).SelectMany(o2 => client.GetStringAsync(o2.Url).ToObservable().Select(str3 => 
+                    r2.Matches(str3).Cast<Match>()
+                        .Select(m => HttpUtility.HtmlDecode(m.Groups.Cast<Group>().ElementAt(1).Value))
+                        .SelectMany((s, i) => r5.Replace(r4.Replace(r3.Replace(s, "").Replace("// RxJS v6+\n", ""), ""), "$1 $2")
+                            .Split('\n').SkipWhile(s2 => string.IsNullOrWhiteSpace(s2))
+                            .Select(s2 => "    " + s2).StartWith($"  {o2.Name}{i + 1}() {{").Concat(new[] { "  }", "" })
+                            .ToList()
+                        ).ToList()
+                )).SelectMany(o2 => o2).ToList()
+            ).Subscribe(examples =>
+                File.WriteAllLines($"/Users/zhaowei/Documents/Programs/Tools/{o.Name}.ts", examples)
+            ));
             Console.ReadKey();
         }
     }
